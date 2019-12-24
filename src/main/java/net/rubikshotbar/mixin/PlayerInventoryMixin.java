50a6e1bde9c1e1d75c687e13ec2f72b6baac957b
Jonathan Schuster <jschuzter@gmail.com>
2019-12-25 00:46:17 +1000

    added hoizontal scrolling, use restriction and general refactoring

--- src/main/java/net/rubikshotbar/mixin/PlayerInventoryMixin.java
+++ src/main/java/net/rubikshotbar/mixin/PlayerInventoryMixin.java
@@ -32,33 +32,70 @@ public abstract class PlayerInventoryMixin {
     @Final
     public PlayerEntity player;
 
+    private long rubikshotbar$lastScroll;
+
     @Environment(EnvType.CLIENT)
     @Inject(method = "scrollInHotbar", at = @At("HEAD"), cancellable = true)
-    private void onScrollInHotbar(double scrollAmount, CallbackInfo ci) {
+    private void rubikshotbar$onScrollInHotbar(double scrollAmount, CallbackInfo ci) {
         if (MinecraftClient.getInstance().options.keySneak.isPressed()) {
-            ArrayList<Integer> slots = new ArrayList<>();
-            int scrollDirection = (int) (scrollAmount / abs(scrollAmount));
-            slots.add(selectedSlot + 18 + 9 * scrollDirection);
-            slots.add(selectedSlot + 18);
-            slots.add(selectedSlot + 18 - 9 * scrollDirection);
+//          Stops players from using a freewheel mouse and causing a huge amount of packets to be sent
+            if (rubikshotbar$hasSufficientTimePassed()) {
+                int scrollDirection = (int) (scrollAmount / abs(scrollAmount));
+                ClientPlayerInteractionManager interactionManager = MinecraftClient.getInstance().interactionManager;
 
-            ClientPlayerInteractionManager interactionManager = MinecraftClient.getInstance().interactionManager;
-            if (interactionManager != null) {
-                for (Integer slot : slots) {
-                    rubikshotbar$swapHotbar(interactionManager, slot);
+                if (MinecraftClient.getInstance().options.keySprint.isPressed()) {
+                    rubikshotbar$doHorizontalScroll(interactionManager, scrollDirection);
+                } else {
+                    rubikshotbar$doVerticalScroll(interactionManager, scrollDirection);
                 }
-            } else {
-//              Can this even happen?
-                System.out.print("Scroll Hotbar failed. No Interaction Manager found!");
             }
             ci.cancel();
         }
+
     }
 
     @Environment(EnvType.CLIENT)
     private void rubikshotbar$swapHotbar(ClientPlayerInteractionManager interactionManager, int slot) {
         if (!main.get(slot).isEmpty()) {
             interactionManager.clickSlot(0, slot, selectedSlot, SlotActionType.SWAP, player);
+        }
+    }
+
+    @Environment(EnvType.CLIENT)
+    private boolean rubikshotbar$hasSufficientTimePassed() {
+        if (System.nanoTime() - rubikshotbar$lastScroll > 50000000) {
+            rubikshotbar$lastScroll = System.nanoTime();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Environment(EnvType.CLIENT)
+    private void rubikshotbar$doVerticalScroll(ClientPlayerInteractionManager interactionManager, int scrollDirection) {
+        ArrayList<Integer> slots = new ArrayList<>();
+        slots.add(selectedSlot + 18 + 9 * scrollDirection);
+        slots.add(selectedSlot + 18);
+        slots.add(selectedSlot + 18 - 9 * scrollDirection);
+
+        if (interactionManager != null) {
+            for (Integer slot : slots) {
+                rubikshotbar$swapHotbar(interactionManager, slot);
+            }
+        } else {
+//              Can this even happen?
+            System.out.print("Scroll Hotbar failed. No Interaction Manager found!");
+        }
+    }
+
+    @Environment(EnvType.CLIENT)
+    private void rubikshotbar$doHorizontalScroll(ClientPlayerInteractionManager interactionManager, int scrollDirection) {
+        if (interactionManager != null) {
+            for (int i = scrollDirection > 0 ? 0 : 9; i <= 9 && i >= 0; i += scrollDirection) {
+                interactionManager.clickSlot(0, 28, i, SlotActionType.SWAP, player);
+            }
+        } else {
+            System.out.print("Scroll Hotbar failed. No Interaction Manager found!");
         }
     }
 }
